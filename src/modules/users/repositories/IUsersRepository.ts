d6072ed7c1ba443132a1ea9d19ab81e5dc6a2e06
Andre gois <andre.s.gois3@gmail.com>
2021-09-20 21:32:57 -0300

    adding properties to the user model

--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -1,4 +1,4 @@
-import { User } from "../model/User";
+import { User } from '../model/User';
 
 interface ICreateUserDTO {
   name: string;
@@ -6,7 +6,7 @@ interface ICreateUserDTO {
 }
 
 interface IUsersRepository {
-  create({ name, email }: ICreateUserDTO): User;
+  create({ name, email }: ICreateUserDTO): void;
   findById(id: string): User | undefined;
   findByEmail(email: string): User | undefined;
   turnAdmin(user: User): User;
